138b1210a727e9eb0568837820fb761cb0101dab
Dave Mount <davemount@gmail.com>
2022-10-30 10:28:20 -0700

    Off to a good start

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Zanzibar's World of Nuclear Power
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>on Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -51,47 +51,17 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
     title: 'Forum',
-    caption: 'forum.quasar.dev',
+    caption: 'The Making of Zanzi World',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: 'https://zanzisworldofnuclearpower.locals.com/'
   },
   {
     title: 'Twitter',
-    caption: '@quasarframework',
+    caption: '@ZanzibarNuclear',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://twitter.com/ZanzibarNuclear'
   },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ]
 
 export default defineComponent({
