ff470b2e8485572d8e2418436a8b5b0ced5eeb83
Dave Mount <davemount@gmail.com>
2022-11-06 17:09:16 -0800

    Got WhatSayYou component improvement

---
+++ src/components/SimpleEditor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-editor
+      v-model="editor"
+      paragraph="p"
+      min-height="5rem"
+      placeholder="Type whatever you like..."
+    />
+    <q-btn label="Post" @click="handlePost" />
+    <q-btn label="Click" @click="handleClick" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'SimpleEditor',
+  props: {
+    initialMessage: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['post', 'click-it'],
+  methods: {
+    handlePost() {
+      this.$emit('post', this.editor)
+      this.editor = ''
+    },
+    handleClick() {
+      this.$emit('click-it')
+    }
+  },
+  data() {
+    return {
+      editor: ref(this.initialMessage),
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/components/WhatSayYou.vue
+++ src/components/WhatSayYou.vue
@@ -3,29 +3,31 @@
     Hi. I am Zanzibar, and I can teach you about nuclear power. Who are you?
   </div>
   <div class="q-pa-md" style="width: 100%; max-width: 500px">
-    <WhatSayYouEditor :editor="message" />
-    <q-input v-model="message" outlined autogrow />
-    <q-btn @click="handleAddMessage">Send</q-btn>
-    <q-scroll-area style="height: 200px">
-      <q-list separator>
-        <q-item v-for="msg in whatsayyou.messages" :key="msg.createdOn">{{
-          msg.message
-        }}</q-item>
-      </q-list>
-    </q-scroll-area>
+    <SimpleEditor
+      initial-message="Blargy pants are fun to wear."
+      @post="handlePost"
+      @click-it="handleClick"
+    />
+    <q-card
+      flat
+      bordered
+      v-for="entry in whatsayyou.messages"
+      :key="entry.createdOn"
+    >
+      <q-card-section><div v-html="entry.message" /></q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script setup>
-import WhatSayYouEditor from './WhatSayYouEditor.vue'
+import SimpleEditor from './SimpleEditor.vue'
 import { useWhatSayYouStore } from 'stores/whatsayyou-store.js'
-import { ref } from 'vue'
-
 const whatsayyou = useWhatSayYouStore()
-const message = ref('Get this party started')
-const handleAddMessage = () => {
-  whatsayyou.addMessage(message.value)
-  message.value = ''
+const handleClick = () => {
+  alert('clicked it yo.')
+}
+const handlePost = (msg) => {
+  whatsayyou.addMessage(msg)
 }
 </script>
 
