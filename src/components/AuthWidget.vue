65fcaaf6c5f1ca1c8c90637020baac85d0291610
Dave Mount <davemount@gmail.com>
2022-11-27 16:20:46 -0800

    Troubleshoot signup and verify in AuthWidget

--- src/components/AuthWidget.vue
+++ src/components/AuthWidget.vue
@@ -1,21 +1,82 @@
 <template>
-  <div></div>
+  <div class="boxy">
+    <q-input v-model="username" label="Username" stack-label :dense="dense" />
+    <q-input
+      v-model="code"
+      label="Verification code"
+      stack-label
+      :dense="dense"
+    />
+    <q-input
+      v-model="password"
+      type="password"
+      label="Password"
+      stack-label
+      :dense="dense"
+    />
+    <q-input
+      v-model="password"
+      :type="isHidePwd ? 'password' : 'text'"
+      label="Password"
+      stack-label
+      :dense="dense"
+    >
+      <template #append>
+        <q-icon
+          :name="isHidePwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isHidePwd = !isHidePwd"
+        />
+      </template>
+    </q-input>
+    <q-input
+      v-model="email"
+      label="Email"
+      stack-label
+      type="email"
+      :dense="dense"
+    />
+    <q-input v-model="nickname" label="Alias" stack-label :dense="dense" />
+    <q-btn-group push>
+      <q-btn label="Sign Up" @click="handleSignUp" />
+      <q-btn label="Sign In" @click="handleSignIn" />
+      <q-btn label="Sign Out" @click="handleSignOut" />
+      <q-btn label="Confirm" @click="handleConfirmSignUp" />
+    </q-btn-group>
+  </div>
 </template>
 
 <script>
 import { reactive, toRefs } from 'vue'
 import { useUserStore } from 'stores/user-store.js'
-import { Auth } from 'aws-amplify';
+import { Auth } from 'aws-amplify'
 
 export default {
   setup() {
     const userStore = useUserStore()
     const state = reactive({
-      count: 0,
+      username: null,
+      password: null,
+      email: null,
+      nickname: null,
+      code: null,
+      isHidePwd: true,
+      dense: true,
+      isSignedIn: false,
     })
     const handleSignUp = async () => {
       try {
-        const { user } = await Auth.signUp({ username, password })
+        const { user } = await Auth.signUp({
+          username: state.username,
+          password: state.password,
+          attributes: {
+            email: state.email,
+            nickname: state.nickname,
+          },
+          autoSignIn: {
+            enabled: true,
+          },
+        })
         console.log(user)
         userStore.onUserSignIn(user)
       } catch (error) {
@@ -25,10 +86,12 @@ export default {
     }
     const handleSignIn = async () => {
       try {
-        const user = await Auth.signIn(username, password)
+        const user = await Auth.signIn(state.username, state.password)
         userStore.onUserSignIn(user)
+        state.isSignedIn = true
       } catch (error) {
         console.log('error signing in', error)
+        state.isSignedIn = false
         alert(error)
       }
     }
@@ -36,9 +99,18 @@ export default {
       try {
         await Auth.signOut()
         userStore.onUserSignOut()
+        state.isSignedIn = false
       } catch (error) {
         console.log('error signing out: ', error)
         alert(error)
+      }
+    }
+    const handleConfirmSignUp = async () => {
+      try {
+        await Auth.confirmSignUp(state.username, state.code)
+        alert('confirmed')
+      } catch (error) {
+        console.log('error confirming sign up', error)
       }
     }
     return {
@@ -46,9 +118,15 @@ export default {
       handleSignUp,
       handleSignIn,
       handleSignOut,
+      handleConfirmSignUp,
     }
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.boxy {
+  border: 1px black solid;
+  padding: 0.5em;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,20 +1,29 @@
 <template>
   <q-page class="flex">
     <div class="q-pa-md">
-      <div>
-        <q-img
-          alt="Zanzibar, Nuclear Hero"
-          src="~assets/Zanzibar.svg"
-          width="200px"
-          height="200px"
-        />
-        <div>
-          I am Zanzibar. I can teach you about nuclear power. What would you
-          like to know?
+      <div class="row headroom">
+        <div class="col">
+          <auth-widget />
         </div>
       </div>
-      <div>
-        <what-say-you />
+      <div class="row headroom">
+        <div class="col">
+          <div>
+            <q-img
+              alt="Zanzibar, Nuclear Hero"
+              src="~assets/Zanzibar.svg"
+              width="200px"
+              height="200px"
+            />
+            <div>
+              I am Zanzibar. I can teach you about nuclear power. What would you
+              like to know?
+            </div>
+          </div>
+          <div>
+            <what-say-you />
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
@@ -23,9 +32,16 @@
 <script>
 import { defineComponent } from 'vue'
 import WhatSayYou from 'src/components/WhatSayYou.vue'
+import AuthWidget from 'src/components/AuthWidget.vue'
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { WhatSayYou },
+  components: { WhatSayYou, AuthWidget },
 })
 </script>
+
+<style lang="scss" scoped>
+.headroom {
+  margin-top: 1em;
+}
+</style>
