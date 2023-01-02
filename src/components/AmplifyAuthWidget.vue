32658c63faf169570c41e776a2586e3a364ac8e1
Dave Mount <davemount@gmail.com>
2023-01-02 14:26:31 -0800

    Customize auth, use preferred_username

--- src/components/AmplifyAuthWidget.vue
+++ src/components/AmplifyAuthWidget.vue
@@ -1,15 +1,21 @@
 <template>
-    <authenticator>
+  <authenticator :sign-up-attributes="['preferred_username']">
     <template #default="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
+      <div class="text-center">
+        <h4>
+          Hello
+          {{ user.attributes.preferred_username || user.attributes.email }}!
+        </h4>
+        <p>({{ user.username }})</p>
+        <button @click="signOut">Sign Out</button>
+      </div>
     </template>
   </authenticator>
 </template>
 
 <script setup>
-  import { Authenticator } from "@aws-amplify/ui-vue";
-  import "@aws-amplify/ui-vue/styles.css";
+import { Authenticator } from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
 </script>
 
 <style lang="scss" scoped></style>
